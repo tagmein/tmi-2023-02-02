de3568b7a944765a1ec4b978e4e2f5269ad26607
Nate Ferrero <nate@tagme.in>
2023-01-31 05:57:53 +0000

    add 4 words

--- tmi.js
+++ tmi.js
@@ -280,6 +280,7 @@ const STATIC = {
 <head>
  <title>Tag Me In</title>
  <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1" />
  <link rel="stylesheet" type="text/css" href="/main.css" />
 </head>
 <body>
